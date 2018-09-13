7af26f820206a35f354ab14af4b7ed5be8de7268
Phil Kulin <schors@gmail.com>
2018-09-13 15:12:29 +0000

    Parser logging improvement

--- parse.sh
+++ parse.sh
@@ -10,18 +10,24 @@ certsdir_new="${certsdir}.new"
 parser=$3
 repodir=`dirname "$certsdir"`
 
+logdate=`date +"%Y-%m-%dT%TZ%z"`
+
+logit() {
+        echo "${logdate}: $1"
+}
+
 if [ x"$certsdir" = "x" -o "$certsdir" = "/" ]; then
-        echo "$certsdir does not exists"
+        logit "[-] $certsdir does not exists"
         exit 0
 fi
 
 if [ x"$certsdir_new" = "x" -o "$certsdir_new" = "/" ]; then
-        echo "$certsdir_new does not exists"
+        logit "[-] $certsdir_new does not exists"
         exit 0
 fi
 
 if [ ! -x "$parser" ]; then
-        echo "$parser not exists or not executable"
+        ligit "[-] $parser not exists or not executable"
         exit 0
 fi
 
@@ -31,27 +37,33 @@ which xmllint > /dev/null || exit 0
 cd $repodir
 
 if wget --quiet -O $caxmlfile_new $url ; then
+        logit "[+] $url was fetched"
         ver1="0"
         if [ -e "$caxmlfile" ]; then
                 ver1=`xmllint --xpath "//АккредитованныеУдостоверяющиеЦентры/Версия/text()" $caxmlfile`
         fi
         ver2=`xmllint --xpath "//АккредитованныеУдостоверяющиеЦентры/Версия/text()" $caxmlfile_new`
         if [ "$ver1" != "$ver2" ]; then
+                logit "[=] Version changed: old=${ver1} new=${ver2}"
                 if [ -d "$certsdir_new" ]; then
                         rm -rf "$certsdir_new"
                 fi
                 mkdir -p "$certsdir_new"
                 if $parser -d "$certsdir_new" -x "$caxmlfile_new" >/dev/null 2>&1 ; then
+                        logit "[+] Parsed successfully"
                         mv -f $caxmlfile_new $caxmlfile
                         rm -rf "$certsdir"
                         mv -f "$certsdir_new" "$certsdir"
+                else
+                        logit "[-] Parser error: $?"
                 fi
         else
-                echo "Version not changed. Nothing to do"
+                logit "[=] Version not changed: old=${ver1} new=${ver2} Nothing to do"
         fi
+else
+        logit "[-] Can't fetch $url: $?"
 fi
 if [ -e "$caxmlfile_new" ]; then
         rm -f $caxmlfile_new
 fi
-
 
